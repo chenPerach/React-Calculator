5f5dbd88664d38a22e216bbfa288820ec1c407fe
chen <chen76273@gmail.com>
2021-07-19 03:53:10 +0300

    new directory

---
+++ src/Calculator.tsx
@@ -0,0 +1,116 @@
+import React, { FC } from "react";
+import "./App.css";
+import {
+  addAction,
+  addSymbol,
+  CalculatorState,
+  eq,
+} from "./redux/features/calculator/CalculatorSlice";
+import { useAppSelector, useCalculatorDispatch } from "./redux/hooks";
+import { RootState } from "./redux/store";
+
+export const Calculator: FC = () => {
+  const dispatch = useCalculatorDispatch();
+  const props: CalculatorState = useAppSelector((state:RootState) => state.calculator);
+
+  return (
+    <div className="calculator">
+      <CalcScreen
+        currentValue={props.currentValue}
+        prevValue={props.prevValue}
+      />
+      <button onClick={()=>dispatch(addAction("+"))} className="cal_button action" id="add">
+        +
+      </button>
+      <button onClick={()=>dispatch(addAction("-"))} className="cal_button action" id="sub">
+        -
+      </button>
+      <button onClick={()=>dispatch(addAction("*"))} className="cal_button action" id="mul">
+        x
+      </button>
+      <button onClick={()=>dispatch(addAction("/"))} className="cal_button action" id="dev">
+        %
+      </button>
+      <button onClick={()=>dispatch(eq())} className="cal_button action" id="eq">
+        =
+      </button>
+      <button
+        onClick={() => {
+            console.log("here");
+          dispatch(addSymbol("1"));
+        }}
+        className="cal_button text"
+      >
+        1
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("2"))}
+        className="cal_button text"
+      >
+        2
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("3"))}
+        className="cal_button text"
+      >
+        3
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("4"))}
+        className="cal_button text"
+      >
+        4
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("5"))}
+        className="cal_button text"
+      >
+        5
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("6"))}
+        className="cal_button text"
+      >
+        6
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("7"))}
+        className="cal_button text"
+      >
+        7
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("8"))}
+        className="cal_button text"
+      >
+        8
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("9"))}
+        className="cal_button text"
+      >
+        9
+      </button>
+      <button
+        onClick={() => dispatch(addSymbol("."))}
+        className="cal_button text"
+        id="dot"
+      >
+        .
+      </button>
+    </div>
+  );
+};
+
+const CalcScreen: FC<CalculatorState> = ({
+  currentValue,
+  prevValue,
+  action,
+}: CalculatorState) => {
+  return (
+    <div id="screen">
+      <div className="screen_content">{currentValue}</div>{" "}
+      <div className="screen_content prev">{prevValue}</div>
+    </div>
+  );
+};
---
+++ src/redux/features/calculator/CalculatorSlice.ts
@@ -0,0 +1,75 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
+
+export interface CalculatorState {
+  prevValue?: string;
+  currentValue: string;
+  action?: string;
+}
+
+const initialState: CalculatorState = {
+  currentValue: "",
+};
+
+export const calculatorSlice = createSlice({
+  name: "calculator",
+  initialState,
+  reducers: {
+    addSymbol: (state, action: PayloadAction<string>) => {
+      if (action.payload === ".") {
+        state.currentValue += action.payload;
+        return;
+      }
+      state.currentValue += action.payload;
+      state.currentValue = Number.parseFloat(state.currentValue).toString();
+    },
+    addAction: (state, action: PayloadAction<string>) => {
+      if (state.prevValue && state.action) {
+        state.prevValue = preformAction(
+          action.payload,
+          Number(state.prevValue),
+          Number(state.currentValue)
+        ).toFixed(4);
+        state.currentValue = "";
+        state.action = "";
+      } else {
+        state.prevValue = state.currentValue;
+        state.action = action.payload;
+        state.currentValue = "";
+      }
+    },
+    eq: (state) => {
+      if (state.prevValue && state.currentValue && state.action) {
+        state.prevValue = state.prevValue = preformAction(
+          state.action,
+          Number(state.prevValue),
+          Number(state.currentValue)
+        ).toFixed(4);
+        state.action = "";
+        state.currentValue = "";
+      }
+    },
+  },
+});
+
+function preformAction(action: string, val1: number, val2: number) {
+  var rtv = 0;
+  switch (action) {
+    case "*":
+      rtv = val1 * val2;
+      break;
+    case "/":
+      rtv = val1 / val2;
+      break;
+    case "+":
+      rtv = val1 + val2;
+      break;
+    case "-":
+      rtv = val1 - val2;
+      break;
+    default:
+      break;
+  }
+  return rtv;
+}
+export const { addAction, addSymbol, eq } = calculatorSlice.actions;
