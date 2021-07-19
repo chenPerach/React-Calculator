af12dc598d45ba75504c2bec76eaccb85a26d2f5
chen <chen76273@gmail.com>
2021-07-19 04:30:51 +0300

    hello

--- src/redux/features/calculator/CalculatorSlice.ts
+++ src/redux/features/calculator/CalculatorSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { act } from "react-dom/test-utils";
+// import { act } from "react-dom/test-utils";
 
 export interface CalculatorState {
   prevValue?: string;
